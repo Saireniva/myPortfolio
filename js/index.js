6144267de6479dee5ea30921fbee2a280b74c582
Saireniva <saireniva@gmail.com>
2019-07-05 09:18:50 -0700

    4th

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
+// $(document).ready(function(){
+//   // $('#content').pushpin({
+//   //   top: $('#content').offset().top
+//   // });
+//   $('.scrollspy').scrollSpy({
+//     scrollOffset: 0
+//   });
+// });
+
 $(document).ready(function(){
-  $('#content').pushpin({
-    top: $('#content').offset().top
-  });
-  $('.scrollspy').scrollSpy({
-    scrollOffset: 0
-  });
+  $('.fixed-action-btn').floatingActionButton();
 });
 
